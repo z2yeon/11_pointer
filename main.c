f6feac2a1bf1d738b32f03347043a5d350f620ac
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-11-12 15:17:33 +0900

    11_01

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char *pc;
+	int *pi;
+	double *pd;
+	
+	pc=(char*)10000;
+	pi=(int*)10000;
+	pd=(double*)10000;
+	
+	printf("증가 전: pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	
+	printf("증가 후: pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
+	
+	return 0;
+}
